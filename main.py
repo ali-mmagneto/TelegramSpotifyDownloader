9d5ccfdf07f1f3c0abf2c2d0d4e7eb468e7fa6e6
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-02-13 18:04:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -79,6 +79,12 @@ def get_single_song(bot, update):
     else:
         logging.log(logging.INFO, 'sent')
 
+@app.on_message(filters.command("start"))
+async def start(client, message):
+    await client.send_message(message.chat.id, f"""
+**Merhaba**\nBen Bir Spotify İndiriciyim Benim sayemde Spotify Linklerini Telegrama Yükleyebilirsin.\nÖrnek: `https://open.spotify.com/playlist/37i9dQZF1DX5H8QSpChffy?si=JjOBJbrYSq-aHtDGloW4Ag`""")
+
+
 
 def authenticate(bot, update):
     username = update.message.chat.username
